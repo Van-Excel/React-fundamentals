cf1452c01cb67cffadac9746775541666b155631
Van-Excel <66915264+Van-Excel@users.noreply.github.com>
2023-06-25 21:38:18 +0100

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import MyApp from './Doc.js';
 import Condition from './Condition.js';
+import Lists from './Lists.js';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <h1>Hello StackBlitz!</h1>
       <MyApp />
       <Condition />
+      <Lists />
     </div>
   );
 }
---
+++ src/Lists.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//rendering of lists
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+
+export default function Lists() {
+  const listItems = products.map(function (product) {
+    return <li key={product.id}>{product.title} </li>;
+  });
+  return <ul>{listItems}</ul>;
+  //throws an error about using objects as children without ul. why??
+}
